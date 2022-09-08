0fb6a57899813c4b50d262e0cf313a80c0486cba
tsthuy <104844853+tsthuy@users.noreply.github.com>
2022-09-08 13:22:42 +0700

    Update active.js

--- active.js
+++ active.js
@@ -1,7 +1,7 @@
 var a = prompt("Tài Khoản");
 var b = prompt("Mật Khẩu");
 
-if((a=="Tran Thi My Duyen" )&&( b=="09092003" || b=="992003") || b=="09/09/2003" || b=="9/9/2003") {
+if((a=="TranThiMyDuyen" )&&( b=="09092003" || b=="992003") || b=="09/09/2003" || b=="9/9/2003") {
     window.location.href = "start.html"
 } else {
     alert("Your Information is wrong");
